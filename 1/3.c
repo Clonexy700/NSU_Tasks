3d5cdd11deded0a4b663976e654f07c640738314
Clonexy700 <55646391+Clonexy700@users.noreply.github.com>
2022-02-10 22:30:00 +0700

    Update 3.c

--- 1/3.c
+++ 1/3.c
@@ -1,67 +1,63 @@
-#include <stdio.h>
-#include <math.h>
-#include <stdlib.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
 
 int main()
-{
-int a, b, c;
-float discriminant, x1, x2;
-
-//freopen("input.txt", "r", stdin);
-//freopen("output.txt", "w", stdout);
-
-scanf("%d %d %d", &a, &b, &c);
-
-a = (double) a;
-b = (double) b;
-c = (double) c;
-
-
-discriminant = b*b-4*a*c;
-
-printf("discriminant raven %lf", discriminant);
-
-if (a == 0 && b == 0 & c == 0) {
-  printf("MANY");
-  return 0;
-}
-
-if(discriminant < 0)
-    printf("NO");
-
-if (a==0)
     {
-        if(b==0)
-            printf("ERROR");
-        else
+
+        float a,b,c,d,x1,x2;
+
+        freopen("input.txt","r",stdin);
+        freopen("output.txt","w",stdout);
+
+        scanf("%f %f %f ",&a,&b,&c);
+
+
+
+        if (a==0 && b==0 && c==0) {
+          printf("MANY");
+          return 0;
+        }
+
+        d=b*b-4*a*c;
+
+        if(d<0) {
+            printf("NO");
+            return 0;
+          }
+
+        if (a==0)
             {
-                x1=-c/b;
-                printf("1\n");
-                printf("%.5f", x1);
+                if(b==0)
+                    printf("ERROR");
+                else
+                    {
+                        x1=-c/b;
+                        printf("1\n");
+                        printf("%.5f",x1);
+                    }
             }
+        else
+        {
+            if (d==0)
+            {
+                x1=-b/(2*a);
+
+                printf("1\n");
+                printf("%.5f",x1);
+            }
+
+
+        if (d>0)
+            {
+                d=sqrt(d);
+                x1=(-b+d)/(2*a);
+                x2=(-b-d)/(2*a);
+
+                printf("2\n");
+                printf("%.5f %.5f",x1,x2);
+            }
+        }
+
+
     }
-else
-{
-    if (discriminant==0)
-    {
-        x1=-b/(2*a);
-
-        printf("1\n");
-        printf("%.5f",x1);
-    }
-
-
-if (discriminant > 0)
-    {
-        discriminant=sqrt(discriminant);
-        x1=(-b+discriminant)/(2*a);
-        x2=(-b-discriminant)/(2*a);
-
-        printf("2\n");
-        printf("%.5f %.5f",x1,x2);
-    }
-}
-
-return 0;
-
-}
