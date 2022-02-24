7839d5a70d8e2bf4a5462bcb5d018c645426e726
Clonexy700 <55646391+Clonexy700@users.noreply.github.com>
2022-02-24 18:06:57 +0700

    Update 2.c

--- 3. Линейные массивы/2.c
+++ 3. Линейные массивы/2.c
@@ -1,25 +1,36 @@
 #include <stdio.h>
-int main(){
-  int N;
-  freopen("input.txt","r",stdin);
-  freopen("output.txt","w",stdout);
-  scanf("%d", &N);
-  int a[N+1];
-   for(int i=0; i<=N; i++){
-       a[i] = i;
-   }
-   a[1]=0;
-   for(int step=2; step<=N; step++){
-       if(a[step]!=0){
-           for(int j=step*2; j<N; j+=step){
-               a[j]=0;
-           }
-       }
-   }
-   for(int i=0; i<=N; i++){
-       if(a[i]!=0){
-            printf("%d ", a[i]);
-       }
-   }
-   return 0;
+
+int main()
+{
+    int number,i,j;
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    scanf("%d",&number);
+    int array[number+1];
+    for(i = 2; i<=number; i++)
+        array[i] = i;
+
+    i = 2;
+    while ((i*i) <= number)
+    {
+        if (array[i] != 0)
+        {
+            for(j=2; j<number; j++)
+            {
+                if (array[i]*j > number)
+                    break;
+                else
+                    array[array[i]*j]=0;
+            }
+        }
+        i++;
+    }
+
+    for(i = 2; i<=number; i++)
+    {
+        if (array[i]!=0)
+            printf("%d ", array[i]);
+    }
+
+    return 0;
 }
