aef0d21b633f29cdcaf17a40094f869981b6487c
Clonexy700 <55646391+Clonexy700@users.noreply.github.com>
2022-03-04 14:19:50 +0700

    fs

--- 5. Литерный тип/3.c
+++ 5. Литерный тип/3.c
@@ -2,30 +2,19 @@
 #include <stdlib.h>
 #include <string.h>
 
-int findSum(char str[100000])
-{
-    char temp[100000] = "";
-
-    int sum = 0;
-    for (char ch : str) {t
-        if (isdigit(ch))
-            temp += ch;
-
-        else {
-            sum += atoi(temp.c_str());
-            temp = "";
-        }
-    }
-    return sum + atoi(temp.c_str());
-}
-
-int main()
-{
-    char str[100000];
+int main() {
+    char str[1000];
     freopen("input.txt","r",stdin);
     freopen("output.txt","w",stdout);
     gets(str);
-    printf("%d", findSum(str));
-
+    int size = strlen(str);
+    str[size-1] = '\0';
+    int x = atoi(str);
+    if (x%15==0) {
+      printf("YES");
+    }
+    else if (x%15 != 0) {
+      printf("NO");
+    }
     return 0;
 }
