e81d12b85da1d59e70e53d3110b172c922a912b3
Clonexy700 <55646391+Clonexy700@users.noreply.github.com>
2022-03-04 11:42:05 +0700

    Create 2.c

---
+++ 4. Функции и Сортировки/2.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+
+void quicksort(int number[],int first,int last){
+  int i, j, pivot, temp;
+  if(first<last)  {
+    pivot=first;
+    i=first;
+    j=last;
+    while(i<j)  {
+      while(number[i]<=number[pivot]&&i<last)
+        i++;
+      while(number[j]>number[pivot])
+        j--;
+      if(i<j){
+        temp=number[i];
+        number[i]=number[j];
+        number[j]=temp;
+      }
+    }
+    temp=number[pivot];
+    number[pivot]=number[j];
+    number[j]=temp;
+    quicksort(number,first,j-1);
+    quicksort(number,j+1,last);
+  }
+}
+
+int main(){
+  freopen("input.txt","r",stdin);
+  freopen("output.txt","w",stdout);
+  int i, count;
+  scanf("%d",&count);
+  int number[count];
+  for(i=0;i<count;i++)
+    scanf("%d",&number[i]);
+  quicksort(number,0,count-1);
+  for(i=0;i<count;i++)
+  printf("%d ",number[i]);
+  return 0;
+
+}
