cd0a80756a6c92d46977a3088c4bcee25a994a30
Clonexy700 <55646391+Clonexy700@users.noreply.github.com>
2022-03-04 15:09:01 +0700

    Update 4.c

--- 5. Литерный тип/4.c
+++ 5. Литерный тип/4.c
@@ -1,25 +1,29 @@
+#include <iostream>
 #include <bits/stdc++.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 using namespace std;
-int calculate_sum(string str)
+int findSum(string str)
 {
-	string temp = "";
-	int sum = 0;
-	for (char ch: str)
-	{
-		if (isdigit(ch))
-			temp += ch;
-		else
-		{
-			sum += atoi(temp.c_str());
-			temp = "";
-		}
-	}
-	return sum + atoi(temp.c_str());
+    string temp = "";
+    int sum = 0;
+    for (char ch : str) {
+        if (isdigit(ch))
+            temp += ch;
+        else {
+            sum += atoi(temp.c_str());
+            temp = "";
+        }
+    }
+    return sum + atoi(temp.c_str());
 }
 int main()
 {
-	string str;
-	cin >> str;
-	cout << calculate_sum(str);
-	return 0;
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    string str;
+    cin>>str;
+    cout << findSum(str);
+    return 0;
 }
