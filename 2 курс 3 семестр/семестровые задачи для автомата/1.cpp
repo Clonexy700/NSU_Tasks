736b65c1d5c0233dc2482c4a7ab51fcec25f5d08
Clonexy <conexy700@gmail.com>
2022-09-23 20:38:33 +0700

    матрички

--- 2 курс 3 семестр/семестровые задачи для автомата/1.cpp
+++ 2 курс 3 семестр/семестровые задачи для автомата/1.cpp
@@ -1,264 +1,172 @@
-// C++ program to illustrate the
-// above functionalities
-#include <cstring>
 #include <iostream>
+#include <vector>
 using namespace std;
 
-// Class Mystring
-class Mystring {
-
-    // Prototype for stream insertion
-    friend ostream&
-    operator<<(
-        ostream& os,
-        const Mystring& obj);
-
-    // Prototype for stream extraction
-    friend istream& operator>>(
-        istream& is,
-        Mystring& obj);
-
-    // Prototype for '+'
-    // operator overloading
-    friend Mystring operator+(
-        const Mystring& lhs,
-        const Mystring& rhs);
-    char* str;
-
+class Matrix {
+private:
+    int size{};
+    vector<vector<int>> matrix;
 public:
-    // No arguments constructor
-    Mystring();
+    // Конструкторы
+    Matrix(): size(1) {}
+    Matrix(const Matrix  &matrix) = default;
+    Matrix(int size, int diagonal_element);
+    explicit Matrix(int size): size(size) {}
 
-    // pop_back() function
-    void pop_bk();
+    // Overload операторов
+    Matrix operator+(Matrix &) const;
+    Matrix operator-(Matrix &) const;
+    Matrix operator*(Matrix &) const;
+    Matrix transpose();
+    ~Matrix();
 
-    // push_back() function
-    void push_bk(char a);
+    int& operator()(const int &RowNumeral, const int &ColNumeral);
 
-    // To get the length
-    int get_length();
+    int getSize() const;
 
-    // Function to copy the string
-    // of length len from position pos
-    void copy(char s[], int len, int pos);
+    void init();
 
-    // Swap strings function
-    void swp(Mystring& rhs);
+    void diagonalize(int k);
 
-    // Constructor with 1 arguments
-    Mystring(char* val);
+    void print();
 
-    // Copy Constructor
-    Mystring(const Mystring& source);
-
-    // Move Constructor
-    Mystring(Mystring&& source);
-
-    // Overloading the assignment
-    // operator
-    Mystring& operator=(
-        const Mystring& rhs);
-
-    // Destructor
-    ~Mystring() { delete str; }
+    void fill();
 };
 
-// Overloading the assignment operator
-Mystring& Mystring::operator=(
-    const Mystring& rhs)
-{
-    if (this == &rhs)
-        return *this;
-    delete[] str;
-    str = new char[strlen(rhs.str) + 1];
-    strcpy(str, rhs.str);
-    return *this;
-}
-
-// Overloading the plus operator
-Mystring operator+(const Mystring& lhs,
-                   const Mystring& rhs)
-{
-    int length = strlen(lhs.str)
-                 + strlen(rhs.str);
-
-    char* buff = new char[length + 1];
-
-    // Copy the strings to buff[]
-    strcpy(buff, lhs.str);
-    strcat(buff, rhs.str);
-    buff[length] = '\0';
-
-    // String temp
-    Mystring temp{ buff };
-
-    // delete the buff[]
-    delete[] buff;
-
-    // Return the concatenated string
-    return temp;
-}
-// Overloading the stream
-// extraction operator
-istream& operator>>(istream& is,
-                    Mystring& obj)
-{
-    char* buff = new char[1000];
-    memset(&buff[0], 0, sizeof(buff));
-    is >> buff;
-    obj = Mystring{ buff };
-    delete[] buff;
-    return is;
-}
-
-// Overloading the stream
-// insertion operator
-ostream& operator<<(ostream& os,
-                    const Mystring& obj)
-{
-    os << obj.str;
-    return os;
-}
-
-// Function for swapping string
-void Mystring::swp(Mystring& rhs)
-{
-    Mystring temp{ rhs };
-    rhs = *this;
-    *this = temp;
-}
-
-// Function to copy the string
-void Mystring::copy(char s[], int len,
-                   int pos)
-{
-    for (int i = 0; i < len; i++) {
-        s[i] = str[pos + i];
+Matrix Matrix::operator+(Matrix &B) const {
+    Matrix sum(size);
+    sum.init();
+    for (int i = 0; i < size; ++i) {
+        for (int j = 0; j < size; ++j) {
+            sum(i, j) = this->matrix[i][j] + B(i, j);
+        }
     }
-    s[len] = '\0';
+    return sum;
 }
 
-// Function to implement push_bk
-void Mystring::push_bk(char a)
-{
-    // Find length of string
-    int length = strlen(str);
-
-    char* buff = new char[length + 2];
-
-    // Copy character from str
-    // to buff[]
-    for (int i = 0; i < length; i++) {
-        buff[i] = str[i];
+Matrix Matrix::operator-(Matrix &B) const {
+    Matrix diff(size);
+    diff.init();
+    for (int i = 0; i < size; ++i) {
+        for (int j = 0; j < size; ++j) {
+            diff(i, j) = this->matrix[i][j] - B(i, j);
+        }
     }
-    buff[length] = a;
-    buff[length + 1] = '\0';
-
-    // Assign the new string with
-    // char a to string str
-    *this = Mystring{ buff };
-
-    // Delete the temp buff[]
-    delete[] buff;
+    return diff;
 }
 
-// Function to implement pop_bk
-void Mystring::pop_bk()
-{
-    int length = strlen(str);
-    char* buff = new char[length];
-
-    // Copy character from str
-    // to buff[]
-    for (int i = 0; i < length - 1; i++)
-        buff[i] = str[i];
-    buff[length-1] = '\0';
-
-    // char a to string str
-    *this = Mystring{ buff };
-
-    // delete the buff[]
-    delete[] buff;
-}
-
-// Function to implement get_length
-int Mystring::get_length()
-{
-    return strlen(str);
-}
-
-// with no arguments
-Mystring::Mystring()
-    : str{ nullptr }
-{
-    str = new char[1];
-    str[0] = '\0';
-}
-
-// with one arguments
-Mystring::Mystring(char* val)
-{
-    if (val == nullptr) {
-        str = new char[1];
-        str[0] = '\0';
+Matrix Matrix::operator*(Matrix &B) const {
+    Matrix multiplication(size);
+    multiplication.init();
+    int temp = 0;
+    for (int i = 0; i < size; ++i) {
+        for (int j = 0; j < size; ++j) {
+            temp = 0;
+            for (int k = 0; k < size; ++k) {
+                temp += matrix[i][k] * B(k, j);
+            }
+            multiplication(i, j) = temp;
+        }
     }
+    return multiplication;
+}
 
-    else {
+Matrix Matrix::transpose() {
+    Matrix trans(size);
+    trans.init();
+    for (int i = 0; i < size; ++i) {
+        for (int j = 0; j < size; ++j) {
+            trans(i, j) = this->matrix[j][i];
+        }
+    }
+    return trans;
+}
 
-        str = new char[strlen(val) + 1];
+Matrix::~Matrix()= default;
 
-        // Copy character of val[]
-        // using strcpy
-        strcpy(str, val);
-        str[strlen(val)] = '\0';
+int Matrix::getSize() const {
+    return this->size;
+}
+
+int &Matrix::operator()(const int &RowNumeral, const int &ColNumeral) {
+    return this->matrix[RowNumeral][ColNumeral];
+}
+
+void Matrix::init() {
+    matrix.resize(size);
+    for (int i=0;i<size; i++) {
+        matrix[i].resize(size);
     }
 }
 
-// Copy Constructor
-Mystring::Mystring(const Mystring& source)
-{
-    str = new char[strlen(source.str) + 1];
-    strcpy(str, source.str);
+void Matrix::fill() {
+    for (int i=0; i<size;i++) {
+        for (int j=0; j<size;j++) {
+            cin >> matrix[i][j];
+        }
+    }
 }
 
-// Move Constructor
-Mystring::Mystring(Mystring&& source)
-{
-    str = source.str;
-    source.str = nullptr;
+void Matrix::print() {
+    for (int i=0;i<size; i++) {
+        for (int j=0; j<size; j++) {
+            cout << matrix[i][j] << " ";
+        }
+        cout << endl;
+    }
 }
 
-// Driver Code
-int main()
-{
-    Mystring a;
-    char temp[] = "Hello";
-    Mystring b{ temp };
-    Mystring c{ a };
+void Matrix::diagonalize(int k) {
+    matrix.resize(size);
+    for (int i=0;i<size; i++) {
+        matrix[i].resize(size);
+    }
+    for (int i=0; i<size;i++) {
+        for (int j=0; j<size;j++) {
+            if (i == j) {
+                matrix[i][j] = k;
+                }
+        }
+    }
+}
 
-    char temp1[] = "World";
-    Mystring d{ Mystring{ temp } };
-    b.pop_bk();
-    cout << "Mystring b: "
-         << b << endl;
-    b.push_bk('o');
-    cout << "Mystring b: "
-         << b << endl;
-    cout << "Length of Mystring b: "
-         << b << endl;
+Matrix::Matrix(int size, int diagonal_element) {
+    this->size = size;
+    matrix.resize(size);
+    for (int i=0;i<size; i++) {
+        matrix[i].resize(size);
+    }
+    for (int i=0; i<size;i++) {
+        for (int j=0; j<size;j++) {
+            if (i == j) {
+                matrix[i][j] = diagonal_element;
+            }
+        }
+    }
+}
 
-    char arr[80];
-    b.copy(arr, 3, 0);
-    cout << "arr is: "
-         << arr << endl;
-    d.swp(b);
-
-    cout << d << " "
-         << b << endl;
-
-    d = b + b;
-    cout << "string d: "
-         << d << endl;
-
-    return 0;
+int main() {
+    int n, k;
+    cin >> n;
+    cin >> k;
+    Matrix A(n);
+    A.init();
+    A.fill();
+    Matrix B(n);
+    B.init();
+    B.fill();
+    Matrix C(n);
+    C.init();
+    C.fill();
+    C = C.transpose();
+    Matrix D(n);
+    D.init();
+    D.fill();
+    D = D.transpose();
+    Matrix K(n, k);
+    Matrix result;
+    result = B * C;
+    result = result + A + K;
+    result = result * D;
+    result.print();
 }
